c3e2b64c6059884a2b16fdaf449ad9993da14e28
Ahmed Tarek <122125653+ahmed-tarek-2004@users.noreply.github.com>
2024-09-25 23:16:50 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -32,15 +32,53 @@ product.addEventListener("click",function () {
     location.href="../pages/products.html#product"
 });
 
+
+let review1 = document.getElementById("Review1");
+console.log(review1);
+review.addEventListener("click",function () {
+    console.log("clicked");
+    let clasees = document.getElementsByClassName("active1");
+    console.log(clasees);
+    for (let i = 0; i < clasees.length; i++) {
+        if (clasees[i].className == "active1") {
+            clasees[i].classList.remove("active1");
+            console.log("YES");
+        }
+    }
+    review1.className = "active1";
+    location.href="../pages/products.html#product"
+});
+
+
+let product1 = document.getElementById("Product1");
+console.log(product1);
+product.addEventListener("click",function () {
+    console.log("clicked");
+    let clasees = document.getElementsByClassName("active1");
+    console.log(clasees);
+    for (let i = 0; i < clasees.length; i++) {
+        if (clasees[i].className == "active1") {
+            clasees[i].classList.remove("active1");
+            console.log("YES");
+        }
+    }
+    product1.className = "active1";
+    location.href="../pages/products.html#product"
+});
+
 window.addEventListener("scroll",()=>{
     if (window.scrollY>=800)
     {
         review.className="active";
         product.classList.remove("active");
+        review1.className="active1";
+        product1.classList.remove("active1");
     }
     else
     {
         product.className="active";
         review.classList.remove("active");
+        product1.className="active1";
+        review1.classList.remove("active1");
     }
 })
