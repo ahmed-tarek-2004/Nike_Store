57a52b61d6d5af06a293e699bbdc3e8db77501e2
Ahmed Tarek <122125653+ahmed-tarek-2004@users.noreply.github.com>
2024-09-25 23:17:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,16 +65,91 @@ services.addEventListener("click",function () {
     }
     services.className = "active";
 });
+let about1 = document.getElementById("About1");
+console.log(about1);
+about1.addEventListener("click",function () {
+    console.log("clicked");
+    let clasees = document.getElementsByClassName("active1");
+    console.log(clasees);
+    for (let i = 0; i < clasees.length; i++) {
+        if (clasees[i].className == "active1") {
+            clasees[i].classList.remove("active1");
+            console.log("YES");
+        }
+    }
+    about1.className = "active1";
+});
 
-// window.addEventListener("scroll",()=>{
-//     if (window.scrollY>=800)
-//     {
-//         about.className="active";
-//         home.classList.remove("active");
-//     }
-//     else
-//     {
-//         home.className="active";
-//         about.classList.remove("active");
-//     }
-// })
+
+let home1 = document.getElementById("Home1");
+console.log(home1);
+home1.addEventListener("click",function () {
+    console.log("clicked");
+    let clasees = document.getElementsByClassName("active1");
+    console.log(clasees);
+    for (let i = 0; i < clasees.length; i++) {
+        if (clasees[i].className == "active1") {
+            clasees[i].classList.remove("active1");
+            console.log("YES");
+        }
+    }
+    home1.className = "active1";
+});
+
+
+let services1=document.getElementById("service1")
+console.log(services);
+services1.addEventListener("click",function () {
+    console.log("clicked");
+    let clasees = document.getElementsByClassName("active1");
+    console.log(clasees);
+    for (let i = 0; i < clasees.length; i++) {
+        if (clasees[i].className == "active1") {
+            clasees[i].classList.remove("active1");
+            console.log("YES");
+        }
+    }
+    services1.className = "active1";
+});
+
+window.addEventListener("scroll",()=>{
+    if (window.scrollY>=800)
+    {
+        about.className="active";
+        home.classList.remove("active");
+    }
+    else
+    {
+        home.className="active";
+        about.classList.remove("active");
+    }
+})
+window.addEventListener("scroll",()=>{
+    if (window.scrollY>=800&&window.scrollY<=1500)
+    {
+        about.className="active";
+        home.classList.remove("active");
+        services.classList.remove("active");
+        about1.className="active1";
+        home1.classList.remove("active1");
+        services1.classList.remove("active1");
+    }
+    else if (window.scrollY>1500)
+    {
+        services.className="active";
+        home.classList.remove("active");
+        about.classList.remove("active");
+        services1.className="active1";
+        home1.classList.remove("active1");
+        about1.classList.remove("active1");
+    }
+    else
+    {
+        home.className="active";
+        about.classList.remove("active");
+        services.classList.remove("active");
+        home1.className="active1";
+        about1.classList.remove("active1");
+        services1.classList.remove("active1");
+    }
+})
